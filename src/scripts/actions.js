e65cca9a6578677987655f0b8f748b6063168265
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-20 12:30:26 -0500

    wow look at all these changes

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -3,18 +3,29 @@ import {User} from './models/models'
 const ACTIONS = {
   registerUser: function(userObj){
     User.register(userObj).then(()=>this.logUserIn(userObj.email,
-      userObj.password))
+      userObj.password),
+      (err)=>{
+        alert('FAILURE to register')
+        console.log(err)
+      }
+    )
   },
   logUserIn: function(email, password){
     User.login(email, password).then(
       (resp)=> {
         alert(`user ${email} logged in!`)
         console.log(resp)
+        location.hash= "home"
       },
       (err)=>{
-        alert('FAILURE')
+        alert('FAILURE logging in')
         console.log(err)
       })
+  },
+  logUserOut: function(){
+    User.logout().then(
+      ()=> location.hash="login"
+    )
   }
 }
 
