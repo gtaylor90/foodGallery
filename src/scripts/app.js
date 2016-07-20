0ef87ea16946ac6f062eb81fb079cfe7424472b3
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-20 11:15:22 -0500

    added routes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import init from './init'
+import LoginView from './views/LogInView.js'
 
 
 const app = function() {
-  document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+  const AppRouter = Backbone.Router.extend({
+    routes: {
+      "home": "handleHome",
+      "dish/postDishes": "handlePostDish",
+      "dish/myDishes": "handleMyDishes",
+      "login": "handleLogin",
+      "*catchall": "default"
+    },
+    
+  })
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
