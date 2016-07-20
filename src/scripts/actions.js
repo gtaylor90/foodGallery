a4db897fcac53bf96b5fbfefa667758ea0221d4f
gtaylor90 <graham.taylor.90@gmail.com>
2016-07-20 12:18:21 -0500

    log in bullshit

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -2,7 +2,19 @@ import {User} from './models/models'
 
 const ACTIONS = {
   registerUser: function(userObj){
-    User.register(userObj)
+    User.register(userObj).then(()=>this.logUserIn(userObj.email,
+      userObj.password))
+  },
+  logUserIn: function(email, password){
+    User.login(email, password).then(
+      (resp)=> {
+        alert(`user ${email} logged in!`)
+        console.log(resp)
+      },
+      (err)=>{
+        alert('FAILURE')
+        console.log(err)
+      })
   }
 }
 
